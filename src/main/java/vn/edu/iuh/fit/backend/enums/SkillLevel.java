64daca87c2dc52d10a4d1206c58b29f577ff2a3e
Dam Thanh Lam <ldamthanh62@gmail.com>
2024-12-02 08:48:50 +0700

    App Job board use: spring security,Oauth02 Client(with google), spring MVC, rest API,

---
+++ src/main/java/vn/edu/iuh/fit/backend/enums/SkillLevel.java
@@ -0,0 +1,12 @@
+package vn.edu.iuh.fit.backend.enums;
+
+public enum SkillLevel {
+    MASTER(1),
+    BEGINER(2),
+    ADVANCED(3),
+    PROFESSIONAL(4),
+    IMTERMEDIATE(5);
+    SkillLevel(int level) {
+
+    }
+}
